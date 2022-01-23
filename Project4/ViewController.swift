dd72e2cc87915915934480200cf646935cdf4396
nursah <nursahari@hotmail.com>
2022-01-23 18:51:31 +0300

    100 days of swift project 4 basic browser

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import WebKit
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView : WKWebView!
+    var progressView : UIProgressView!
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        
+        
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for ws in WebSites.allCases {
+                if host.contains(ws.rawValue.lowercased()){
+                    decisionHandler(.allow)
+                    return
+                }
+                
+            }
+            decisionHandler(.cancel)
+        }
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.isNavigationBarHidden = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openBtn))
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let back = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let forward = UIBarButtonItem(title: "Next", style: .plain, target: webView, action: #selector(webView.goForward))
+        toolbarItems = [back, progressButton, forward, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
     }
 
 
+    @objc func openBtn() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        for ws in WebSites.allCases{
+            ac.addAction(UIAlertAction(title: ws.rawValue, style: .default, handler: openPage))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac,animated: true)
+    }
+    
+    func openPage (action : UIAlertAction) {
+        let url = URL(string: "https://" + action.title!.lowercased() + ".com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
 }
 
---
+++ Project4/WebSites.swift
@@ -0,0 +1,16 @@
+//
+//  WebSites.swift
+//  Project4
+//
+//  Created by Nurşah on 23.01.2022.
+//
+
+import Foundation
+
+
+enum WebSites : String,CaseIterable {
+    case Github
+    case HackingWithSwift
+    case Google
+    case Youtube
+}
